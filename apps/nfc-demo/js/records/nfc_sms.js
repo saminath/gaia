10a843db4269c555259cfed75b763f4eb833b76d
Garner Lee <garner.lee@telekom.com>
2012-08-13 17:09:46 -0700

    Moved ndef record js files to records directory.

---
+++ apps/nfc-demo/js/records/nfc_sms.js
@@ -0,0 +1,18 @@
+var nfcSms = {
+
+createSmsNdefRecord: function(sms) {
+  var record = new MozNdefRecord();
+
+  record.tnf = nfc.tnf_well_known;
+  record.type = nfc.rtd_uri;
+  record.id = null;
+  
+  // Payload:
+  var prefix = 0x00; // No Prefix.
+  var payload = String.fromCharCode(prefix) + "sms:"+sms.phoneNumber+"?body="+sms.message;
+  record.payload = payload;
+
+  return record;
+}
+
+}
